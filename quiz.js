f1230bffb6a7b061de5065ec77b57e001b35ce4a
Sarah Shuey <sarah.shuey@me.com>
2017-08-24 10:00:39 -0500

    time up

--- quiz.js
+++ quiz.js
@@ -1,1 +1,53 @@
 console.log('quiz 1');
+function begin() {
+  console.log('begin');
+}
+begin();
+
+function echo(a) {
+  return (a);
+}
+echo('iron yard');
+
+function math(a,b,c) {
+return (a+b*c)
+}
+math(3,4,2);
+
+let words=['hello','world']
+function toString(words) {
+return(
+
+)
+}
+toString(words);
+
+function callMe(){
+  function second() {
+console.log('in second function');
+  }
+console.log('in first function');
+}
+callMe();
+
+
+function callMe2(second,str) {
+  function callMe(second){
+    function second() {
+  console.log('in second function');
+    }
+  console.log('in first function');
+  }
+}
+callMe2('hello')
+
+function reverse(str) {
+return(reverse.str)
+}
+reverse('hello')
+
+
+setTimeout(function sleep () {
+console.log('after sleep loop');
+}, 1000);
+console.log('before sleep loop');
